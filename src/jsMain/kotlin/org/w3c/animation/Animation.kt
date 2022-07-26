7ab68c79fd661fc47a17a526fe771d3b8c80c144
Toshiyuki Ogawa <toshi@oc-soft.net>
2022-07-26 20:44:35 +0900

    You can have extense of kotlin stdlib with this project.

---
+++ src/jsMain/kotlin/org/w3c/animation/Animation.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2022 oc-soft
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.w3c.animation
+
+import org.w3c.dom.events.EventTarget
+
+external abstract class Animation: EventTarget {
+
+    fun play()
+
+    fun reverse() 
+
+    fun cancel()
+
+    fun finish()
+}
+
+// vi: se ts=4 sw=4 et:
+
